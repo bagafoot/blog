30148565714bbe7b624b7b0af0e47f45b3b41f2f
Bekir Niyaz <bagafoot@hotmail.com>
2017-08-08 17:16:43 +0300

    proje altyapısı kuruldu

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Post
 
-# Register your models here.
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title','content','publishing_dttm']
+    search_fields = ['title']
+    list_filter = ['title','publishing_dttm']
--- post/models.py
+++ post/models.py
@@ -1,7 +1,11 @@
 from django.db import models
+from datetime import datetime
 
 class Post(models.Model):
-    baslik = models.CharField(max_length=120)
-    icerik = models.TextField()
-    olusturma_tarih = models.DateTimeField()
+    title = models.CharField(max_length=120,verbose_name="title")
+    content = models.TextField(verbose_name="content")
+    publishing_dttm = models.DateTimeField(verbose_name="publishing date", default=datetime.now())
+
+    def __str__(self):
+        return "{title},{publising}".format(title=self.title,publising=self.publishing_dttm)
 
